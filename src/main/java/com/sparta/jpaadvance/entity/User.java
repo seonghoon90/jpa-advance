a27e112b78be72e13f96f1f15d457f1caeb21805
hoon <dotofi@naver.com>
2024-05-23 11:40:26 +0900

    지연 로딩과 즉시 로딩

--- src/main/java/com/sparta/jpaadvance/entity/User.java
+++ src/main/java/com/sparta/jpaadvance/entity/User.java
@@ -17,6 +17,6 @@ public class User {
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy = "user")
-    private List<Order> orderList = new ArrayList<>();
+    @OneToMany(mappedBy = "user") //FetchType.Lazy
+    private List<Food> foodList = new ArrayList<>();
 }
